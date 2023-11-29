53fd294a364652c8c84ce043fb2119378add4e32
kranthi.007007@gmail.com <kranthi.007007@gmail.com>
2023-11-29 14:17:00 +0530

    Slider box commited

--- Pamper/src/components/Card.js
+++ Pamper/src/components/Card.js
@@ -1,21 +1,24 @@
 import React from 'react'
 
-const Card = ({item}) => {
+const Card = ({ item }) => {
   return (
-    <div className='card card-style-1 border-0' key={item.id}>
-    <div className='card-body p-0'>
-        <div className='image-thumbnail'>
-        <img src={item.thumbnail} className='img-fluid w-100' alt=''  />
-        <span className='icon'>
-          <img src={item.icon} alt='' />
-        </span>
+    <div className="card card-style-1 border-0" key={item.id}>
+      <div className="card-body p-0">
+        <div className="image-thumbnail">
+          <div className='overflow-hidden w-100 h-100'>
+            <img src={item.thumbnail} className="img-fluid w-100 thumb_img" alt="" />
           </div>
-      <div className='card-info'>
-        <h2 className='card-title'>{item.title}</h2>
+
+          <span className="icon">
+            <img src={item.icon} alt=""/>
+          </span>
+        </div>
+
+        <div className="card-info">
+          <h2 className="card-title">{item.title}</h2>
+        </div>
       </div>
     </div>
-   
-</div>
   )
 }
 
